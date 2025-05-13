6bf4a0b157ddc7d2d70a9e5ae9349213b19951a3
Jacob <122307017+JacobHamelMottiez@users.noreply.github.com>
2025-05-13 13:42:37 -0400

    Update prefs.js

--- prefs.js
+++ prefs.js
@@ -3,5 +3,5 @@ pref("extensions.zotero-pdf-background.daytimeColor","#404040");
 pref("extensions.zotero-pdf-background.nighttimeColor","#575757");
 pref("extensions.zotero-pdf-background.careeyeColor","#578f32");
 pref("extensions.zotero-pdf-background.parchmentColor","#937b25");
-pref("extensions.zotero-pdf-background.customColor","#05ee0a");
+pref("extensions.zotero-pdf-background.customColor","#333248");
 pref("extensions.zotero-pdf-background.extraAttributes","opacity:0.3;");
